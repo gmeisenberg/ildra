beeda54705715e5ac6edcc3fc3a05f65df60442f
gmeisenberg <gregmeisenberg@gmail.com>
2025-05-23 03:24:10 -0700

    https://open-meteo.com API

--- site.js
+++ site.js
@@ -1,12 +1,16 @@
-const ipGeoApi = 'https://ipwho.is';
+const ipApi = 'https://ipwho.is';
+const weatherApi = 'https://api.open-meteo.com/v1/forecast?current=temperature_2m,relative_humidity_2m,is_day,precipitation,weather_code,cloud_cover,wind_speed_10m&timezone=auto&forecast_days=1&wind_speed_unit=mph';
 
 async function fetchData() {
   try {
-    const ipGeoResponse = await fetch(ipGeoApi);
-    const ipGeo = await ipGeoResponse.json();
+    const ipResponse = await fetch(ipApi);
+    const ipData = await ipResponse.json();
 
-    printData(ipGeo.ip);
-    console.log(ipGeo);
+    const weatherResponse = await fetch(`${weatherApi}&latitude=${ipData.latitude}&longitude=${ipData.longitude}`);
+    const weatherData = await weatherResponse.json();
+
+    //printData(ipData.ip);
+    console.log(ipData, weatherData);
   } catch (error) {
     console.error('Error:', error);
   }
